0144a266fe4627085380e6dccb3c3a204f01a3c2
imsaifun <imsaifun@gmail.com>
2022-06-22 10:18:26 +0600

    working

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { Temporal } from '@js-temporal/polyfill';
-import { useState } from 'react';
-import TimeUntil from '../Components/TimeUntil';
 import getDailyEntry from '../lib/getDailyEntry';
-import { BASE_URL } from '../lib/BASE_URL';
 // if (!process.env.NEXT_PUBLIC_HOST_URL) { process.env.NEXT_PUBLIC_HOST_URL = process.env.NEXT_PUBLIC_VERCEL_URL; };
 
 export async function getStaticProps() {
