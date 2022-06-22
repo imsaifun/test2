e0ec3bad9f9255be2861758630db1d937ae98e13
imsaifun <imsaifun@gmail.com>
2022-06-22 15:50:59 +0600

    working

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,17 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/PizzaCard.module.css";
 
 const PizzaCard = ({ pizza }) => {
   return (
-    <div className={styles.container}>
-      <img src={pizza.img} alt="" />
-      <h1 className={styles.title}>{pizza.title}</h1>
-      <span className={styles.price}>${pizza.prices[0]}</span>
-      <p className={styles.desc}>
-        {pizza.desc}
-      </p>
-    </div>
+    <Link href={`/product/${pizza._id}`}>
+      <div className={styles.container}>
+        <img src={pizza.img} alt="" />
+        <h1 className={styles.title}>{pizza.title}</h1>
+        <span className={styles.price}>${pizza.prices[0]}</span>
+        <p className={styles.desc}>
+          {pizza.desc}
+        </p>
+      </div>
+    </Link>
   );
 };
 
